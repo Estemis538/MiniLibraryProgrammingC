3537aba0764567e937297bb89c2ced3f1c522497
GalaxyCat <hrdestemis@gmail.com>
2026-07-07 00:50:37 +0500

    Kokochnene

--- myinclude.c
+++ myinclude.c
@@ -5,6 +5,24 @@ void prt_char_arr(char massiv[]){
     }
     printf("\n");
 }
+char return_char_arr(char massiv[]){
+    int i;
+    for(i = 0;massiv[i]!='\0';i++){
+        printf("%c",massiv[i]); 
+    }
+    return i;
+}
+
+void ptr_int_arr(int massiv[],int size){
+    printf("[");
+    for(int i = 0;i<size;i++){
+        printf("%d ",massiv[i]);
+    }
+    printf("]\n");
+}
+
+
+
 
 void compare_strings(char str1[], char str2[]) {
     int i = 0;
@@ -15,5 +33,11 @@ void compare_strings(char str1[], char str2[]) {
         printf("%s = %s\n",str1,str2);
     } else {
         printf("%s != %s\n",str1,str2);
+    }
+}
+
+void copy_int_massiv(int cpmass[],int insertms[],int size){
+    for(int i = 0;i<size;i++){
+        cpmass[i] = insertms[i];
     }
 }
